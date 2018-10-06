c0f0a4eaa47e2236e517e42de99b86578132f20e
ATarsa <smb909@gmail.com>
2018-10-06 16:10:04 +0100

    Add js script to toggle show class, modified css to show menu on click

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function showMenu(x) {
+    x.classList.toggle("change");
+
+    document.getElementById("menu").classList.toggle("show");
+    document.getElementById("navbar").classList.toggle("opaque");
+}
